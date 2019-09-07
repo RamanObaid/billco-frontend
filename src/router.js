aeb3f6af06468576a5064666c15e303eaf05fef5
Raman Obaid <raman.obaid.ahmed@gmail.com>
2019-09-07 12:20:32 +0300

    Added Create, Update, View views for Invoice

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import DashboardLayout from '@/layout/DashboardLayout'
-import AuthLayout from '@/layout/AuthLayout'
 Vue.use(Router)
 
 export default new Router({
@@ -14,52 +13,33 @@ export default new Router({
       children: [
         {
           path: '/dashboard',
-          name: 'dashboard',
           component: () => import(/* webpackChunkName: "demo" */ './views/Dashboard.vue')
         },
         {
-          path: '/profile',
-          name: 'profile',
-          component: () => import(/* webpackChunkName: "demo" */ './views/UserProfile.vue')
-        },
-        {
           path: '/invoices',
-          name: 'invoices',
           component: () => import(/* webpackChunkName: "demo" */ './views/Invoices.vue')
         },
         {
           path: '/create-invoice',
-          name: 'create invoice',
           component: () => import(/* webpackChunkName: "demo" */ './views/Invoice/Create.vue')
         },
         {
+          path: '/invoice-details/quoted',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Invoice/Quoted.vue')
+        },
+        {
+          path: '/invoice-details/cancelled',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Invoice/Cancelled.vue')
+        },
+        {
           path: '/bills',
-          name: 'bills',
           component: () => import(/* webpackChunkName: "demo" */ './views/Bills.vue')
         },
         {
           path: '/companies',
-          name: 'companies',
           component: () => import(/* webpackChunkName: "demo" */ './views/Companies.vue')
         }
       ]
-    },
-    // {
-    //   path: '/',
-    //   redirect: 'login',
-    //   component: AuthLayout,
-    //   children: [
-    //     {
-    //       path: '/login',
-    //       name: 'login',
-    //       component: () => import(/* webpackChunkName: "demo" */ './views/Login.vue')
-    //     },
-    //     {
-    //       path: '/register',
-    //       name: 'register',
-    //       component: () => import(/* webpackChunkName: "demo" */ './views/Register.vue')
-    //     }
-    //   ]
-    // }
+    }
   ]
 })
